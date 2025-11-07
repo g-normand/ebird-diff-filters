d4a7244523767f6c0fb04d158c7e41875bddf3f5
Guillaume Normand <guiguiec@pm.me>
2025-11-07 16:02:36 -0500

    With new taxons

--- verify_filter.py
+++ verify_filter.py
@@ -10,13 +10,23 @@ def extract_observation_dates(file_path):
     for li in soup.find_all('li', class_='BirdList-list-list-item'):
         time_tag = li.find('time')
         if time_tag and time_tag.has_attr('datetime'):
+            is_exotic = li.find('svg', class_='Icon--exoticEscapee') is not None
+            if is_exotic:
+                continue
             ebird_name = li.get('id')
-            result[ebird_name] = datetime.strptime(time_tag['datetime'], '%Y-%m-%d %H:%M')
+            observer_div = li.find('div', class_='Obs-observer')
+            birder_tag = observer_div.select_one('span:not(.is-visuallyHidden), a')  
+            birder = birder_tag.get_text(strip=True) if birder_tag else None
+            has_pictures = li.find('svg', class_='Icon--photo') is not None
+            result[ebird_name] = dict(
+                last_seen=datetime.strptime(time_tag['datetime'], '%Y-%m-%d %H:%M'),
+                birder=birder, 
+                has_pictures=has_pictures)
 
     return result
 
 
-def extract_species_counts(file_path, azuay_dates):
+def extract_species_counts(file_path, azuay_data):
     with open(file_path, encoding='utf-8') as f:
         soup = BeautifulSoup(f, 'html.parser')
 
@@ -36,18 +46,20 @@ def extract_species_counts(file_path, azuay_dates):
             	value = lim_span.get_text(strip=True)
             else:
             	value = input_span.get('value')
-            species_map[name] = dict(value=int(value))
             ebird_name = li.get('id').replace('_out', '')
-            if ebird_name in azuay_dates:
-            	species_map[name]['last_seen'] = azuay_dates[ebird_name]
+            if ebird_name in azuay_data:
+            	species_map[name] = azuay_data[ebird_name]
+            else:
+                species_map[name] = dict()
+            species_map[name]['value'] = int(value)
 
     return species_map
     
  
 
 
-azuay_dates = extract_observation_dates('azuay_list.html')
-azuay_counts = extract_species_counts('azuay_filter.html', azuay_dates)
+azuay_data = extract_observation_dates('azuay_list.html')
+azuay_counts = extract_species_counts('azuay_filter.html', azuay_data)
 
 sorted_data = dict(sorted(
     azuay_counts.items(),
